e53542a194c324e725a5b484b204556dff0130b2
kim <kim_man@live.com>
2021-08-09 21:55:58 +1000

    added more styling to the navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { FaBars } from "react-icons/fa"
 import { Link } from "gatsby"
 import styled from "styled-components"
 
@@ -6,17 +7,18 @@ const Header = () => {
   return (
     <Nav>
       <NavLink to="/">BOULDER</NavLink>
+      <Bars />
     </Nav>
   )
 }
 
-//YT 26:08
+//YT 30:00
 
 export default Header
 
 const Nav = styled.nav`
   background: tomato;
-  height: 80x;
+  height: 80px;
   display: flex;
   justify-content: space-between;
   padding: 0.5rem calc((100vw-1300px) / 2);
@@ -31,4 +33,17 @@ const NavLink = styled(Link)`
   padding: 0 1rem;
   height: 100%;
   cursor: pointer;
+`
+const Bars = styled(FaBars)`
+  color: #fff;
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
 `
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,11 @@
 import * as React from "react"
 import Header from "./header"
+import { GlobalStyle } from "./styles/GlobalStyle"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <GlobalStyle />
       <Header />
 
       <main>{children}</main>
