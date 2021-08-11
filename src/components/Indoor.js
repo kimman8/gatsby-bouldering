4c2e3fc73487507b05a127737d3f4be1a5b56ec1
kim <kim_man@live.com>
2021-08-11 21:20:20 +1000

    added pics to indoor pg

--- src/components/Indoor.js
+++ src/components/Indoor.js
@@ -1,11 +1,60 @@
 import React from "react"
+import { ImLocation } from "react-icons/im"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 import styled from "styled-components"
+import { Button } from "./Button"
 
 const Indoor = () => {
+  const data = useStaticQuery(graphql`
+    query IndoorQuery {
+      allIndoorJson {
+        edges {
+          node {
+            alt
+            button
+            name
+            img {
+              childImageSharp {
+                fluid(maxWidth: 3080, quality: 100) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  function getIndoor(data) {
+    const indoorArray = []
+    data.allIndoorJson.edges.forEach((item, index) => {
+      indoorArray.push(
+        <ProductCard key={index}>
+          <ProductImg
+            src={item.node.img.childImageSharp.fluid.src}
+            alt={item.node.alt}
+            fluid={item.node.img.childImageSharp.fluid}
+          />
+          <ProductInfo>
+            <TextWrap>
+              <ImLocation />
+              <ProductTitle>{item.node.name}</ProductTitle>
+            </TextWrap>
+            <Button to="/indoor" big round>
+              {item.node.button}
+            </Button>
+          </ProductInfo>
+        </ProductCard>
+      )
+    })
+    return indoorArray
+  }
   return (
     <ProductsContainer>
       <ProductsHeading>Heading</ProductsHeading>
-      <ProductWrapper>Wrapper</ProductWrapper>
+      <ProductWrapper>{getIndoor(data)}</ProductWrapper>
     </ProductsContainer>
   )
 }
@@ -24,4 +73,55 @@ const ProductsHeading = styled.div`
   margin-bottom: 5rem;
   color: #000;
 `
-const ProductWrapper = styled.div``
+const ProductWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 10px;
+  justify-items: center;
+  padding: 0 2rem;
+
+  @media screen and (max-width: 1200px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media screen and (max-width: 868px) {
+    grid-template-columns: 1fr;
+  }
+`
+const ProductCard = styled.div`
+  line-height: 2;
+  width: 100%;
+  height: 500px;
+  position: relative;
+  transition: 0.2s ease;
+`
+const ProductImg = styled(Img)`
+  max-width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  position: relative;
+  filter: brightness(70%);
+  transition: 0.4s cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  &:hover {
+    filter: brightness(100%);
+  }
+`
+const ProductInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 0 2rem;
+
+  @media screen and (max-width: 280px) {
+    padding: 0 1rem;
+  } ;
+`
+const TextWrap = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: 375px;
+`
+const ProductTitle = styled.div``
